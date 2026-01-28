34ca183c97dd9a223a00f482aa1b6f75ebbda867
middlegear <otukojoshua@outlook.com>
2026-01-28 12:17:01 +0300

    refactor: add logs on kwik to see whats happening

--- src/source-extractors/kwik.ts
+++ src/source-extractors/kwik.ts
@@ -83,19 +83,30 @@ class Kwik extends BaseClass {
       const kwikLinkRegex = /https?:\/\/kwik\.cx\/f\/[a-zA-Z0-9]+/i;
       const kwikLink = kwikFile.data.match(kwikLinkRegex)?.[0];
       if (!kwikLink) throw new Error('Could not find Kwik F-link');
+      console.log(kwikLink);
 
       // 2. Load the download page (Impit handles cookies automatically)
 
       // Capture the cookie properly
       const setCookie = kwikFile.headers['set-cookie'];
       const cookie = Array.isArray(setCookie) ? setCookie.join('; ') : setCookie;
-      // console.log(cookie);
+
+      console.log('PHPSESSID=', cookie);
 
       const res2 = await this.client2.fetch(kwikLink, {
         method: 'GET',
         headers: { Referer: videoUrl.href, Cookie: cookie },
       });
+
+      if (!res2.ok) {
+        const errorBody = await res2.text();
+        throw new Error(`Kwik RES2 GET: ${res2.status} - ${errorBody}`);
+      }
+
       const html2 = await res2.text();
+
+      console.log('obfuscated HTML containing token', html2);
+
       const cookieArray = res2.headers.getSetCookie();
       const cookieString = cookieArray.join('; ');
 
