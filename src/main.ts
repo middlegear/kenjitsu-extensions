365a6d446a1a4cbc72db2c714b5d713a80c85d90
middlegear <97509851+middlegear@users.noreply.github.com>
2026-07-31 14:33:32 +0300

    chore: remove jikan since its shutting down

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { TheMovieDatabase } from './provider/meta/tmdb.js';
 import { Anizone } from './provider/anime/anizone.js';
 import type { IMetaFormat, Seasons } from './types/meta/meta-anime.js';
 import type { IAnimeCategory } from './types/base.js';
-import { Jikan } from './provider/meta/jikan.js';
 import { Anikoto } from './provider/anime/anikoto.js';
 import { AniDB } from './provider/anime/anidb.js';
 import { AnimeHeaven } from './provider/anime/animeheaven.js';
@@ -14,7 +13,6 @@ import { Kitsu } from './provider/meta/kitsu.js';
 export {
   Anilist,
   TheMovieDatabase,
-  Jikan,
   Anizone,
   Anikoto,
   AniDB,
@@ -26,6 +24,3 @@ export {
   type Seasons,
   type IAnimeCategory,
 };
-
-// const data = await new Anilist().fetchAnizoneProviderId(210031);
-// console.log(data);
